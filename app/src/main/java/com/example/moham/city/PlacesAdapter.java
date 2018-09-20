2e339cec8b82e37b99e889bc581ddb7ec2c67a85
routeproject <routeproject2018@gmail.com>
2018-09-20 03:06:50 +0200

    Test

--- app/src/main/java/com/example/moham/city/PlacesAdapter.java
+++ app/src/main/java/com/example/moham/city/PlacesAdapter.java
@@ -75,7 +75,7 @@ public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder
     public  interface OnItemClickListener{
          void onClick(int position, Place place);
          //test
-        //get this2
+        //get this266
     }
 
 }
